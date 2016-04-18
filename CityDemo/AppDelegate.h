97c2b85972e952cbd7b1005dd094b1b03f2ff538
李海瑞 <lihairui1@mac-mini.local>
2016-04-18 15:15:55 +0800

    Initial Commit

---
+++ CityDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityDemo
+//
+//  Created by 李海瑞 on 16/4/18.
+//  Copyright © 2016年 李海瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CityDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CityDemo
+//
+//  Created by 李海瑞 on 16/4/18.
+//  Copyright © 2016年 李海瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
